183d63a46821d3eb2ab8152ce727deb80da936cf
Mateo Hernandez <mateo.hernandezr@udea.edu.co>
2023-02-18 12:52:57 -0500

    Commit realizado

---
+++ quickstart.py
@@ -0,0 +1,36 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+
+# <code>
+import azure.cognitiveservices.speech as speechsdk
+
+# Creates an instance of a speech config with specified subscription key and service region.
+# Replace with your own subscription key and service region (e.g., "westus").
+speech_key, service_region = "b3420608878c4cd08ba56249075e0cc2", "eastus"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+# Set the voice name, refer to https://aka.ms/speech/voices/neural for full list.
+speech_config.speech_synthesis_voice_name = "es-BO-SofiaNeural"
+
+# Creates a speech synthesizer using the default speaker as audio output.
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+# Receives a text from console input.
+print("Escribe el texto...")
+text = input()
+
+# Synthesizes the received text to speech.
+# The synthesized speech is expected to be heard on the speaker with this line executed.
+result = speech_synthesizer.speak_text_async(text).get()
+
+# Checks result.
+if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized to speaker for text [{}]".format(text))
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+    print("Did you update the subscription info?")
+# </code>
